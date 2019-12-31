140654238392417fde58bae2897db50d52a1e0b4
Ajay Kumar <imajkumar@hotmail.com>
2019-12-31 19:03:46 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//import { Button } from 'antd';
+import { Button } from 'evergreen-ui'
+
+import 'antd/dist/antd.css';
 
 const App = () => {
-  return <div>Hello React,Webpack 4 & Babel 7! welcome</div>;
+  return <div>
+      Hello React,Webpack 4 & Babel 7! welcome
+      
+      <Button>I am using 🌲 Evergreen!</Button>,
+      </div>;
 };
 ReactDOM.render(<App />, document.querySelector("#root"));
